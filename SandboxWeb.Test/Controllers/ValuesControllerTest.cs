b828b21b1b9d8c0d7cd3c8ca95d4bc807185b0b0
Mark Williams <mark.williams@groupgti.com>
2018-11-02 14:45:19 +0000

    Initial project

---
+++ SandboxWeb.Test/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using SandboxWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SandboxWeb.Test.Controllers
+{
+    public class ValuesControllerTest
+    {
+        private readonly ValuesController valuesController;
+
+        public ValuesControllerTest()
+        {
+            valuesController = new ValuesController();
+        }
+
+        [Fact]
+        public void ShouldGetList()
+        {
+            var result = valuesController.Get();
+
+            var values = result.Value;
+
+            Assert.Contains(values, v => v.Equals("value1"));
+            Assert.Contains(values, v => v.Equals("value2"));
+        }
+
+        [Fact]
+        public void ShouldGetSingle()
+        {
+            var result = valuesController.Get(5);
+
+            var value = result.Value;
+
+            Assert.Equal("value", value);
+        }
+    }
+}
